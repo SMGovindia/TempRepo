f6a8b589f1f8da587c43546c1256d9b5a85340d0
Saurabh Govindia <saurabh.govindia@globant.com>
2022-01-17 12:03:01 +0530

    Add function added.

--- HelloWorld_2.cpp
+++ HelloWorld_2.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
+int add(int, int);
+
 int main()
 {
 	cout << "Hello World 2 New Changes!!!" << endl;
+	add(4, 5);
 	return 0;
+}
+
+int add(int a, int b)
+{
+	int c = a + b;
+	return c;
 }
